8174f009980ba5a518f9dc7475d6c33fb06f2adf
moroccan-coder <tssryassine@gmail.com>
2020-11-03 16:05:21 -0800

    start Profile3

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 //import 'package:flutter_ui/profiles/profile1/profile_1.dart';
 import 'package:flutter_ui/profiles/profile2/profile_2.dart';
+import 'package:flutter_ui/profiles/profile3/profile_3.dart';
 
 void main()
 {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     //home: Profile1(),
-    home: Profile2(),
+    //home: Profile2(),
+    home: Profile3(),
   ));
 }
---
+++ lib/profiles/profile3/profile_3.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Profile3 extends StatefulWidget {
+  @override
+  _Profile3State createState() => _Profile3State();
+}
+
+class _Profile3State extends State<Profile3> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
